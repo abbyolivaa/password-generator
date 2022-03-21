3a43fd51f5a1eac19d1a0a40b4c4a5f6c84f02fa
Abigail Oliva <abigailoliva@192.168.1.18>
2022-03-21 13:47:45 -0600

    let toggle

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 
-// #region variables
+// region variables
 let lowercase = false;
 let uppercase = false;
 let numeric = false;
@@ -9,10 +9,48 @@ const animationTime = 65;
 let animationI = 0;
 
 const Characters = {
-  lowercase: 'abcdefghijklmnopqrstuvwyxyz'
-  uppercase 
+  lowercase: 'abcdefghijklmnopqrstuvwyxyz',
+  uppercase:'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numeric: '0123456789',
+  special: '!"#$%&\'( )*+,-./:;<=>?@[\\]^_`{|}~'
+};
+// end region
 
+let FadeToggle = (targetElement) => {
+  if(Math.fround((document.getElementsByClassName(targetElement)[0].getAttribute("style").split(";")[1].split(":")[1]).trim()) == 0) {
+    let animation = setInterval(()=>{
+      if(animationI >= animationTime){
+          document.getElementsByClassName(targetElement)[0].setAttribute("style", "display: flex; opacity: 1;");
+          clearInterval(animation);
+          animationI = 0;
+      } else{
+        let opacityPerUpdate = Math.fround((1 / animationTime));
+        
+      
+        const currentOpacity = Math.fround((document.getElementsByClassName(targetElement)[0].getAttribute("style").split(";")[1].split(":")[1]).trim());
+        document.getElementsByClassName(targetElement)[0].setAttribute("style", "display: flex; opacity: " + (currentOpacity + opacityPerUpdate));
+
+        animationI++;
+      }
+    }, 1);
+  } else {
+    let animation = setInterval(()=>{
+      if(animationI >= animationTime){
+          document.getElementsByClassName(targetElement)[0].setAttribute("style", "display: none; opacity: 0;");
+          clearInterval(animation);
+          animationI = 0;
+      } else{
+        let opacityPerUpdate = Math.fround((1 / animationTime));
+
+        const currentOpacity = Math.fround((document.getElementsByClassName(targetElement)[0].getAttribute("style").split(";")[1].split(":")[1]).trim());
+        document.getElementsByClassName(targetElement)[0].setAttribute("style", "display: flex; opacity: " + (currentOpacity - opacityPerUpdate));
+        
+        animationI++;
+      }
+    }, 1);
+  }
 }
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
