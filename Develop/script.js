fcb8ac325569cb672e64f70d8fe576c091fe6552
Abigail Oliva <abigailoliva@192.168.1.18>
2022-03-21 13:19:40 -0600

    added popup to html file

---
+++ Develop/script.js
@@ -0,0 +1,31 @@
+// Assignment code here
+
+// #region variables
+let lowercase = false;
+let uppercase = false;
+let numeric = false;
+let special = false;
+const animationTime = 65;
+let animationI = 0;
+
+const Characters = {
+  lowercase: 'abcdefghijklmnopqrstuvwyxyz'
+  uppercase 
+
+}
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+ function writePassword() {
+  function generatePassword(){
+    var password = generatePassword();
+  }
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
